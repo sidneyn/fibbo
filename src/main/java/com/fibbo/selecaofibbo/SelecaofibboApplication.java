22e896f76ab53d7f42d1d3de9ff408d2bbfc7fea
sidneyn <sidneyn@gmail.com>
2024-07-16 09:34:44 -0300

    Projeto iniciado selecao fibbo

---
+++ src/main/java/com/fibbo/selecaofibbo/SelecaofibboApplication.java
@@ -0,0 +1,13 @@
+package com.fibbo.selecaofibbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaofibboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelecaofibboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fibbo/selecaofibbo/SelecaofibboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fibbo.selecaofibbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelecaofibboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
